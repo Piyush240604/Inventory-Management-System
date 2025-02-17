1120796f6865a6a46d1e02983bfa3a0b6a93fb7b
Piyush Prakash Wakpaijan <piyushprakashwakpaijan@gmail.com>
2025-02-17 15:14:45 +0530

    Update create_superuser.py

--- inventory/management/commands/create_superuser.py
+++ inventory/management/commands/create_superuser.py
@@ -5,14 +5,19 @@ class Command(BaseCommand):
     help = 'Create a superuser with only a username and password'
 
     def handle(self, *args, **kwargs):
+        self.stdout.write(self.style.SUCCESS('Starting superuser creation...'))
+
         username = 'admin'  # Username you want to set
         password = 'password123'  # Password you want to set
 
         # Check if the user already exists
         if not User.objects.filter(username=username).exists():
+            self.stdout.write(self.style.SUCCESS('Creating superuser...'))
             # Create the superuser
             user = User.objects.create_superuser(username=username, password=password)
             user.save()
             self.stdout.write(self.style.SUCCESS(f'Successfully created superuser with username: {username}'))
         else:
             self.stdout.write(self.style.WARNING('Superuser already exists'))
+
+        self.stdout.write(self.style.SUCCESS('Superuser creation process finished.'))
